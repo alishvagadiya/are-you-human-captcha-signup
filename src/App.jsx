ea9f25e27c19d87270d3206a2fdee7008d4eb2cc
Alish Vagadiya <alishvagadiya2202@gmail.com>
2023-01-16 00:45:42 +0530

    captcha img added, validated captcha functionality added, UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "./styles.css";
-// import { response } from "./captcha";
-// import { useState } from "react";
+import { response } from "./captcha-img";
+import { useState } from "react";
 
 export default function App() {
+  const { url, ans } = response.captcha[1];
+  const [captchaInputAns, setCaptchaInputAns] = useState("");
+  const [error, setErrorMsg] = useState("");
+
+  async function signupHandler(e) {
+    e.preventDefault();
+    console.log(ans, captchaInputAns);
+    if (ans === captchaInputAns) {
+      setErrorMsg("Captcha Matched");
+    } else {
+      setErrorMsg("Captcha ans not matching");
+    }
+  }
+
   return (
     <div className="container">
       <div className="box">
@@ -17,16 +31,17 @@ export default function App() {
         </div>
         <div className="captchaBox">
           <div className="imgWrap">
-            <img
-              className="img"
-              alt="captcha"
-              src="https://www.pandasecurity.com/en/mediacenter/src/uploads/2014/09/avoid-captcha.jpg"
-            />
+            <img className="img" alt="captcha" src={url} />
           </div>
-          <input type="text" />
+          <input
+            type="text"
+            value={captchaInputAns}
+            onChange={(e) => setCaptchaInputAns(() => e.target.value)}
+          />
         </div>
+        <div className="error">{error}</div>
         <div className="inputBox">
-          <input type="submit" />
+          <input type="submit" onClick={(e) => signupHandler(e)} />
         </div>
       </div>
     </div>
---
+++ src/captcha-img/index.js
@@ -0,0 +1,52 @@
+import captcha1 from "./c-1323.jpg";
+import captcha2 from "./c-1817.jpg";
+import captcha3 from "./c-4343.jpg";
+import captcha4 from "./c-5659.jpg";
+import captcha5 from "./c-5677.jpg";
+import captcha6 from "./c-7292.jpg";
+import captcha7 from "./c-7669.jpg";
+import captcha8 from "./c-8542.jpg";
+export const response = {
+  captcha: [
+    {
+      id: 1,
+      url: captcha1,
+      ans: 1323
+    },
+    {
+      id: 2,
+      url: captcha2,
+      ans: 1817
+    },
+    {
+      id: 3,
+      url: captcha3,
+      ans: 4343
+    },
+    {
+      id: 4,
+      url: captcha4,
+      ans: 5659
+    },
+    {
+      id: 5,
+      url: captcha5,
+      ans: 5677
+    },
+    {
+      id: 6,
+      url: captcha6,
+      ans: 7292
+    },
+    {
+      id: 7,
+      url: captcha7,
+      ans: 7669
+    },
+    {
+      id: 8,
+      url: captcha8,
+      ans: 8542
+    },
+  ]
+};
